ef3c7a82a0d80fc18be92ae1376180ccf98d128b
tripj33 <jjjanik3@gmail.com>
2021-01-26 21:25:32 +0000

    sdfasxczdasfs

--- .eleventy.js
+++ .eleventy.js
@@ -42,7 +42,7 @@ module.exports = function (eleventyConfig) {
   });
 
   // Copy Image Folder to /_site
-  eleventyConfig.addPassthroughCopy("src/static/img");
+  eleventyConfig.addPassthroughCopy({"src/static/img": "static/img" });
 
   // Copy favicon to route of /_site
   eleventyConfig.addPassthroughCopy("./src/favicon.ico");
